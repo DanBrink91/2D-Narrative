1a65c1d4f38ea9efb7c076dd92750630136e9d5a
Dan Brinkman <DanBrink91@gmail.com>
2016-02-28 01:47:17 -0500

    forgot assets...

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	// Target to follow
+	public Transform target;
+	// Camera Size
+	public float scale = 4f;
+	// Camera Move Speed
+	public float speed = 0.1f;
+	Camera myCam;
+
+	// Use this for initialization
+	void Start () {
+		myCam = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myCam.orthographicSize = (Screen.height / 100f) / scale;
+
+		if (target) {
+			transform.position = Vector3.Lerp(transform.position, target.position, speed) + new Vector3(0, 0, -10);
+		}
+	}
+}
